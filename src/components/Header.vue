5ca280e4544571e602d9034c7a8bee169ec9ff44
hiro <hirotaka5314@gmail.com>
2020-02-10 14:44:34 +0900

    metaタグやh1の変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,47 @@
 <template>
-  <v-tabs
-    fixed-tabs
-    background-color="warning"
-    dark
-    show-arrows
-    class="wf-roundedmplus1c"
-  >
-    <v-tab to="/">
-      <h1>
-        わくわくフェスティバル
-      </h1>
-    </v-tab>
-    <v-tab to="/participants_dance">
-      コンテスト・アイドルでショー
-    </v-tab>
-    <v-tab to="/companion">
-      愛犬家のみなさまへ
-    </v-tab>
-    <v-tab to="/contact">
-      お問い合わせ
-    </v-tab>
-  </v-tabs>
+  <v-card color="warning" class="wf-roundedmplus1c">
+    <v-card-title class="text-center justify-center pa-0 pt-1">
+      <h1><router-link class="white--text wf-nicomoji" style="text-decoration: none;" to="/">わくわくフェスティバル</router-link></h1>
+    </v-card-title>
+    <v-tabs
+      v-model="tab"
+      background-color="transparent"
+      fixed-tabs
+      center-active
+      dark
+      show-arrows
+      class="wf-roundedmplus1c"
+    >
+      <v-tab
+        v-for="item in items"
+        :key="item.title"
+        :to="item.to"
+      >
+        {{ item.title }}
+      </v-tab>
+    </v-tabs>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data () {
+      return {
+        tab: null,
+        items: [
+          {title: 'コンテスト・アイドルでショー', to: '/participants_dance'},
+          {title: '愛犬家のみなさまへ', to: '/companion'},
+          {title: 'お問い合わせ', to: '/contact'},
+        ],
+      }
+    }
 };
 </script>
 
 <style>
 h1 {
   cursor: pointer;
-  font-size: 16px;
-}
-@media (max-width: 750px) {
-h1 {
-  font-size: 15px;
-}
+  font-size: 27px;
 }
 </style>
