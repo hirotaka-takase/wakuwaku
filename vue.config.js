620a45637848f03a324192bafeebf0b4ae4ceb6d
hiro <hirotaka5314@gmail.com>
2020-02-10 18:37:57 +0900

    prerender-spa-pluginの導入とvue.config.jsに出力先設定

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,31 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   publicPath: './',
   outputDir: 'docs',
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  configureWebpack: () => {
+        return {
+            plugins: [
+                new PrerenderSPAPlugin({
+                    staticDir: path.join(__dirname, 'docs'),
+                    routes: [ '/',
+                              '/dance',
+                              '/idol',
+                              '/outline',
+                              '/participants_dance',
+                              '/participants_idol',
+                              '/companion',
+                              '/contact',
+                              '/form/store',
+                              '/form/trim',
+                              '/form/freemarket'
+                            ]
+                })
+            ]
+        }
+    }
 }
